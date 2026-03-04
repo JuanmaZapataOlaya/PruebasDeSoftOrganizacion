75cd28039c5caa5d0b9bd51677d10f967d4b1ecf
JuanmaZapataOlaya <124525725+JuanmaZapataOlaya@users.noreply.github.com>
2026-03-04 13:44:38 +0000

    pruebas java completadas

--- JuegoJava/juego.test.js
+++ JuegoJava/juego.test.js
@@ -5,29 +5,30 @@ test('intento correcto', () => {
 expect(verificarIntento(42, 42)).toBe("correcto");
 });
 test('intento es muy bajo, pista dice mayor', () => {
-expect(verificarIntento(???, ???)).toBe("mayor"); // completar
+expect(verificarIntento(70, 60)).toBe("mayor"); // completar
 });
 test('intento es muy alto, pista dice menor', () => {
-expect(verificarIntento(???, ???)).toBe("menor"); // completar
+expect(verificarIntento(40, 60)).toBe("menor"); // completar
 });
 test('intento fuera de rango lanza excepción', () => {
-expect(() => verificarIntento(50, ???)).toThrow("El número debe estar entre
-1 y 100");
+expect(() => verificarIntento(50, 101)).toThrow("El intento debe estar entre 100 y 1");
 });
 test('intento no numérico lanza excepción', () => {
-expect(() => verificarIntento(50, ???)).toThrow("El intento debe ser un
-número");
+expect(() => verificarIntento(50, "ochenta")).toThrow("El intento debe ser un numero");
 });
 // ── calcularPuntaje ───────────────────────────────────────
 test('3 intentos o menos dan puntaje 100', () => {
-expect(calcularPuntaje(???)).toBe(???); // completar
+expect(calcularPuntaje(3)).toBe(100); // completar
 });
 test('entre 4 y 6 intentos dan puntaje 75', () => {
+    expect(calcularPuntaje(5)).toBe(75);
 // escribir la prueba completa aquí
 });
 test('entre 7 y 10 intentos dan puntaje 50', () => {
+    expect(calcularPuntaje(8)).toBe(50);
 // escribir la prueba completa aquí
 });
 test('más de 10 intentos dan puntaje 25', () => {
+    expect(calcularPuntaje(11)).toBe(25);
 // escribir la prueba completa aquí
 });
